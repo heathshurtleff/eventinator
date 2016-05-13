ba9a25619be2a67c23643a890a0a4c5dc3951591
Heath Shurtleff <heath.shurtleff@overstock.com>
2016-05-13 10:19:36 -0600

    Feat: Fetching real time route info
    We are now fetching realtime updates to the schedule every minute and displaying the changes.

--- dev/js/transportinator/routeInfoCtrl.js
+++ dev/js/transportinator/routeInfoCtrl.js
@@ -1,6 +1,7 @@
-/*global angular, _ */
+/*global angular, _, $ */
 
-angular.module('transportinator').controller('routeInfoController', ['$scope', '$stateParams', 'routesService', function($scope, $stateParams, routesService) {
+angular.module('transportinator').controller('routeInfoController', ['$scope', '$filter', '$stateParams', 'routesService', function($scope, $filter, $stateParams, routesService) {
+	var $allTimes = $('.route-times-table td');
 	$scope.routeId = $stateParams.id;
 
 	routesService.getStopsForRoute($scope.routeId).then(function(stops) {
@@ -9,11 +10,64 @@ angular.module('transportinator').controller('routeInfoController', ['$scope', '
 		});
 	});
 	routesService.getTripsForRoute($scope.routeId).then(function(trips) {
+		var routeStops = $scope.stops.length;
 		var sortedTrips = _.sortBy(trips, function(trip) {
 			return trip.stoptimes[0].departure_time;
 		});
+		_.each(sortedTrips, function(trip) {
+			var newStopArray = [];
+			if(trip.stoptimes.length !== routeStops) {
+				_.each($scope.stops, function(stop, idx) {
+					var matchingStoptime = _.find(trip.stoptimes, function(stoptime) {
+						return stoptime.stop_id === stop.stop_id;
+					});
+					if(matchingStoptime) {
+						newStopArray[idx] = matchingStoptime;
+					} else {
+						newStopArray[idx] = {departure_time: '--'};
+					}
+				});
+				trip.stoptimes = newStopArray;
+			}
+		});
 		$scope.$apply(function() {
 			$scope.trips = sortedTrips;
+			getRouteUpdate();
 		});
 	});
+
+
+	function getRouteUpdate() {
+		routesService.getRouteUpdate($scope.routeId).then(function(updateData) {
+			$scope.$apply(function() {
+				$scope.lastUpdate = updateData.timestamp;
+			});
+			$allTimes.removeClass('updated-time');
+			_.each(updateData.entities, function(entity) {
+				var update;
+				var tripId;
+				var $tripRow;
+				if(entity.trip_update) {
+					update = entity.trip_update;
+					tripId = update.trip.trip_id;
+					$tripRow = $('tr[data-trip-id*="' + tripId + '"]');
+					_.each(update.stop_time_update, function(stoptime) {
+						var $stopToUpdate = $tripRow.find('td[data-stop-id="' + stoptime.stop_id + '"]');
+						var today = new Date();
+						var estTimezoneOffset = 480;
+						var timezoneOffset = estTimezoneOffset - today.getTimezoneOffset();
+						var newTime;
+						var formattedTime;
+						if(stoptime.departure) {
+							newTime = new Date((stoptime.departure.time.low * 1000) + (timezoneOffset * 60 * 1000));
+							formattedTime = $filter('date')(newTime, 'HH:mm:ss');
+							$stopToUpdate.addClass('updated-time');
+							$stopToUpdate.text(formattedTime);
+						}
+					});
+				}
+			});
+			setTimeout(getRouteUpdate, 6000);
+		});
+	}
 }]);
