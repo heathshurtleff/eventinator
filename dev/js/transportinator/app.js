429c07e28b4bf469c08dc7ddeb6eae8ae2651d02
Heath Shurtleff <heath.shurtleff@overstock.com>
2016-05-24 10:00:12 -0600

    Fix: Reenabling the service worker script

--- dev/js/transportinator/app.js
+++ dev/js/transportinator/app.js
@@ -1,12 +1,12 @@
 /*global angular*/
 
-// if( 'serviceWorker' in navigator) {
-// 	navigator.serviceWorker.register('/sw.js').then(function(reg) {
-// 		console.log('ServiceWorker registration successful with scope: ' + reg.scope);
-// 	}).catch(function(err) {
-// 		console.log('ServiceWorker registration failed: ', err);
-// 	});
-// }
+if( 'serviceWorker' in navigator) {
+	navigator.serviceWorker.register('/sw.js').then(function(reg) {
+		console.log('ServiceWorker registration successful with scope: ' + reg.scope);
+	}).catch(function(err) {
+		console.log('ServiceWorker registration failed: ', err);
+	});
+}
 
 angular.module('transportinator', ['ui.router', 'ngResource']);
 
